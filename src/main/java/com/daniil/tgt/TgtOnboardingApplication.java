4ede640d64635a05a59dd00c05e3922a9966e477
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-11-02 20:57:18 -0500

    init: maven spring boot
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>

---
+++ src/main/java/com/daniil/tgt/TgtOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.daniil.tgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgtOnboardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgtOnboardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniil/tgt/TgtOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniil.tgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgtOnboardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
